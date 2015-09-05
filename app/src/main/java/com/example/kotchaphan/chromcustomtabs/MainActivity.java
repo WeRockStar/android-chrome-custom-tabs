d3a763723957b34f35a3df10406b3ab2e78512c5
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 18:42:25 +0700

    implement tabs service

--- app/src/main/java/com/example/kotchaphan/chromcustomtabs/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/chromcustomtabs/MainActivity.java
@@ -1,37 +1,36 @@
 package com.example.kotchaphan.chromcustomtabs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsService;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CustomTabsServiceConnection customTabsServiceConnection;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void connectCustomTabsService(){
+        String chromePackage = "com.android.chrome";
+        customTabsServiceConnection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
+                
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    
 }
