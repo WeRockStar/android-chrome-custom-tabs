b3198438a88e68efe3a525b5ec14757b08947539
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 18:50:22 +0700

    create tabs session

--- app/src/main/java/com/example/kotchaphan/chromcustomtabs/MainActivity.java
+++ app/src/main/java/com/example/kotchaphan/chromcustomtabs/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.kotchaphan.chromcustomtabs;
 
 import android.content.ComponentName;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsClient;
-import android.support.customtabs.CustomTabsService;
 import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomTabsServiceConnection customTabsServiceConnection;
+    private CustomTabsSession customTabsSession;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,33 @@ public class MainActivity extends AppCompatActivity {
         customTabsServiceConnection = new CustomTabsServiceConnection() {
             @Override
             public void onCustomTabsServiceConnected(ComponentName componentName, CustomTabsClient customTabsClient) {
-                
+
             }
 
             @Override
             public void onServiceDisconnected(ComponentName name) {
-
+                //disconnect
             }
-        }
+        };
+        CustomTabsClient.bindCustomTabsService(this, chromePackage, customTabsServiceConnection);
     }
 
-    
+    public void createCustomTabSession(CustomTabsClient customTabsClient){
+        customTabsSession = customTabsClient.newSession(new CustomTabsCallback(){
+            @Override
+            public void onNavigationEvent(int navigationEvent, Bundle extras) {
+                super.onNavigationEvent(navigationEvent, extras);
+
+                //Then Navigation Event on Custom Tab
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //killer object
+        if(customTabsServiceConnection != null)
+            unbindService(customTabsServiceConnection);
+    }
 }
